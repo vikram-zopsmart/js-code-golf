55f5025fa62c311de278666b558bf5ba89f9bd79
Anurag Lingam <anuragl94@users.noreply.github.com>
2022-02-13 16:07:16 +0530

    Add Match 02 to the list of challenges

---
+++ Match 02/README.md
@@ -0,0 +1,3 @@
+# Let It Be
+
+"Let It Be" is a popular song by The Beatles. However, I think that a lot of lyrics are repetitive. Write a solution that prints the lyrics to the song with the minimal set of characters.
---
+++ Match 02/submissions/base.js
@@ -0,0 +1,38 @@
+function solution() {
+  return `when i find myself in times of trouble
+mother mary comes to me
+speaking words of wisdom, let it be
+and in my hour of darkness
+she is standing right in front of me
+speaking words of wisdom, let it be
+let it be, let it be
+let it be, let it be
+whisper words of wisdom, let it be
+and when the broken-hearted people
+living in the world agree
+there will be an answer, let it be
+for though they may be parted
+there is still a chance that they will see
+there will be an answer, let it be
+let it be, let it be
+let it be, let it be
+yeah, there will be an answer, let it be
+let it be, let it be
+let it be, let it be
+whisper words of wisdom, let it be
+let it be, let it be
+ah, let it be, yeah, let it be
+whisper words of wisdom, let it be
+and when the night is cloudy
+there is still a light that shines on me
+shine on until tomorrow, let it be
+i wake up to the sound of music,
+mother mary comes to me
+speaking words of wisdom, let it be
+let it be, let it be
+let it be, yeah, let it be
+oh, there will be an answer, let it be
+let it be, let it be
+let it be, yeah, let it be
+whisper words of wisdom, let it be`;
+}
