e95c1e87755327cb9fa580e303a5a2ba38dbcc3b
vikram-zopsmart <vikram.mateti@zopsmart.com>
2022-02-25 19:51:38 +0530

    chaged file name and added logic to match 03

--- Match 03/submissions/vikram.mateti.js
+++ Match 03/submissions/vikram.mateti.js
@@ -1,9 +1,9 @@
 function solution(a, b) {
+    a = a.replace(/[^\w]/g, '').toLowerCase();
+    b = b.replace(/[^\w]/g, '').toLowerCase();
     let len1 = a.length;
     let len2 = b.length;
-    
     let str1 = a.split('').sort().join('');
-
     let str2 = b.split('').sort().join('');
     if(str1 === str2){
         return true 
