5c65d321338897acc26005aeecfe770e129bc314
vikram-zopsmart <vikram.mateti@zopsmart.com>
2022-02-25 19:57:37 +0530

    minified match 03

--- Match 03/submissions/vikram.mateti-min.js
+++ Match 03/submissions/vikram.mateti-min.js
@@ -1,5 +1,3 @@
-function solution(a, b) {a = a.replace(/[^\w]/g, '').toLowerCase(); b = b.replace(/[^\w]/g, '').toLowerCase() ; let len1 = a.length; let len2 = b.length; let str1 = a.split('').sort().join(''); let str2 = b.split('').sort().join('');
-if(str1 === str2){ return true} 
-else { return false } }
+function solution(a, b) {a = a.replace(/[^\w]/g, '').toLowerCase(); b = b.replace(/[^\w]/g, '').toLowerCase() ; let len1 = a.length; let len2 = b.length; let str1 = a.split('').sort().join(''); let str2 = b.split('').sort().join(''); if(str1 === str2){ return true} else { return false } }
 
 
