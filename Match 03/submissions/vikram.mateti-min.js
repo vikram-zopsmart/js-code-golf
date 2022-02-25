f78a693fb04b5c2c215eca58b3284b889e204048
vikram-zopsmart <vikram.mateti@zopsmart.com>
2022-02-25 19:58:59 +0530

    removed spaces

--- Match 03/submissions/vikram.mateti-min.js
+++ Match 03/submissions/vikram.mateti-min.js
@@ -1,3 +1,1 @@
 function solution(a, b) {a = a.replace(/[^\w]/g, '').toLowerCase(); b = b.replace(/[^\w]/g, '').toLowerCase() ; let len1 = a.length; let len2 = b.length; let str1 = a.split('').sort().join(''); let str2 = b.split('').sort().join(''); if(str1 === str2){ return true} else { return false } }
-
-
